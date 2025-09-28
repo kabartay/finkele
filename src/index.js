0bd174cd80b699484d82465df4dba9e66924ab9c
kabartay <kabartay@users.noreply.github.com>
2025-09-28 12:29:05 +0000

    deploy: e31876a3184828a894776ece39b21148e870d2c8

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'; // optional
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<></>); // empty render, since HTML is static
